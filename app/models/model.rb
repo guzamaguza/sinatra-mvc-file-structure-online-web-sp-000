eeab1a22051e7d63d5c03e4fa640070ca29c2665
Mark A Guza <guzamaguza@gmail.com>
2020-05-08 05:50:20 +0000

    Automatically backed up by Learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,11 +1,20 @@
-class Model
-
-end
-
 class Dog
 
-  def initialize(name,type,age)
+  attr_accessor :name, :breed, :age
 
+  @@all = []
+
+  def initialize(name,breed,age)
+    @name = name
+    @breed = breed
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+      @@all
   end
 
 end
+
+

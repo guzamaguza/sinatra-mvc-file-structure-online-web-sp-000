6e1230a5924d0da9621937b6c5e85211f397147e
Mark A Guza <guzamaguza@gmail.com>
2020-05-08 05:20:32 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -6,10 +6,10 @@ class Dog
   @@all = []
 
   def initialize(name,breed,age)
-    @name = name
-    @breed = breed
-    @age = age
-    @@all << self
+    self.name = name
+    self.breed = breed
+    self.age = age
+    self.class.all << self
   end
 
   def self.all
